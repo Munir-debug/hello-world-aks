be0fbbc7ef55f1dea2aabd70b3a7a42103e8d9be
Munir-debug <munir@goagilecloud.com>
2025-05-21 16:51:52 +0100

    Refactoring the code

--- infra/main.tf
+++ infra/main.tf
@@ -1,33 +1,27 @@
 provider "azurerm" {
   features {}
-  subscription_id = "dd8ab0e1-d33e-4d66-94e8-7ede9facd8ed"
 }
 resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
-  location = "West US"
+  name     = var.resource_group_name
+  location = var.location
 }
-
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "my-aks"
+  name                = var.aks_cluster_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks-demo"
-
+  dns_prefix          = var.dns_prefix
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2ms"
+    node_count = var.default_node_count
+    vm_size    = var.vm_size
   }
-
   identity {
     type = "SystemAssigned"
   }
 }
-
 resource "azurerm_kubernetes_cluster_node_pool" "pool" {
-  name                = "pool"
+  name                  = var.additional_node_pool_name
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size             = "Standard_B2ms"
-  node_count          = 1
+  vm_size               = var.vm_size
+  node_count            = var.additional_node_count
 }
-
