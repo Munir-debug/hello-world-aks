b2cf3434280f6454e977adb80c2aef2ba0beb094
Munir-debug <munir@goagilecloud.com>
2025-05-21 13:02:49 +0100

    Adding final changes

---
+++ IAC/Azure.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "dd8ab0e1-d33e-4d66-94e8-7ede9facd8ed"
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "my-resource-group"
+  location = "West US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "my-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks-demo"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2ms"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "pool" {
+  name                = "pool"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size             = "Standard_B2ms"
+  node_count          = 1
+}
+
